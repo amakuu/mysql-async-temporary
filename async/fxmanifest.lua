4bec5e6f8f68c1957970cbdaba8e5d5c0c2a981d
Grzegorz Dąbrowski <amakuzaxx@gmail.com>
2019-12-14 13:21:54 +0100

    Delete unused thing

--- async/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version "adamant"
-games {"rdr3"}
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-description 'Async'
-
-server_script 'async.lua'
-client_script 'async.lua'
